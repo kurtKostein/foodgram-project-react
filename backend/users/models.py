e69c5cef283c5f71b14447cd931ae6b594147d6f
Kurt Kostein <kurtkostein@yandex.ru>
2021-07-25 17:56:12 +0300

    refactor login to username in users/models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,9 +8,9 @@ from django.utils import timezone
 
 class CustomUserManager(BaseUserManager):
     def _create_user(
-            self, first_name, last_name, login, email, password, **kwargs
+            self, first_name, last_name, username, email, password, **kwargs
     ):
-        values = [first_name, last_name, login, email, password]
+        values = [first_name, last_name, username, email, password]
         field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
         for field_name, field_value in field_value_map.items():
             if not field_value:
@@ -18,7 +18,7 @@ class CustomUserManager(BaseUserManager):
         user = self.model(
             first_name=first_name,
             last_name=last_name,
-            login=login,
+            username=username,
             email=self.normalize_email(email),
             **kwargs
         )
@@ -27,28 +27,30 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_user(
-            self, first_name, last_name, login, email, password=None, **kwargs
+            self, first_name, last_name, username, email, password=None,
+            **kwargs
     ):
         kwargs.setdefault('is_staff', False)
         kwargs.setdefault('is_superuser', False)
         return self._create_user(
-            first_name, last_name, login, email, password, **kwargs
+            first_name, last_name, username, email, password, **kwargs
         )
 
     def create_superuser(
-            self, first_name, last_name, login, email, password=None, **kwargs
+            self, first_name, last_name, username, email, password=None,
+            **kwargs
     ):
         kwargs.setdefault('is_staff', True)
         kwargs.setdefault('is_superuser', True)
         return self._create_user(
-            first_name, last_name, login, email, password, **kwargs
+            first_name, last_name, username, email, password, **kwargs
         )
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=25, verbose_name='Имя')
     last_name = models.CharField(max_length=25, verbose_name='Фамилия')
-    login = models.CharField(max_length=25, verbose_name='Имя пользователя')
+    username = models.CharField(max_length=25, verbose_name='Имя пользователя')
     email = models.EmailField(
         max_length=255,
         verbose_name='Адрес элетронной почты',
@@ -59,10 +61,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now)
     last_login = models.DateTimeField(null=True)
 
-    object = CustomUserManager()
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'login', 'password']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username', 'password']
 
     def get_full_name(self):
         return f'{self.first_name} {self.last_name}'
