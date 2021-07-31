6b54e3f6c72d93dfe780710b8d327073db89921b
Kurt Kostein <kurtkostein@yandex.ru>
2021-07-31 21:39:02 +0300

    refactor models in recipe/models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,32 +1,38 @@
 # recipes/models.py
 from django.db import models
+from django.core.validators import MinValueValidator
 
-from ..users.models import CustomUser
+from users.models import CustomUser
 
 
-class Ingredient(models.Model):
-    name = models.CharField(verbose_name='Ингридиент', max_length=200)
+class BaseRecipeClass(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class Ingredient(BaseRecipeClass):
     measurement_unit = models.CharField(
         verbose_name='Ед. измерения',
         max_length=200
     )
 
-    def __str__(self):
-        return self.name
 
-
-class Tag(models.Model):
-    name = models.CharField(verbose_name='Тэг', max_length=200)
+class Tag(BaseRecipeClass):
     color = models.CharField(verbose_name='Цвет', max_length=200, null=True)
     slug = models.SlugField(verbose_name='Слаг', max_length=200, null=True)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        verbose_name = 'Тэг'
 
 
-class Recipe(models.Model):
+class Recipe(BaseRecipeClass):
     author = models.ForeignKey(
-        to='CustomUser',
+        CustomUser,
         verbose_name='Автор рецепта',
         on_delete=models.CASCADE
     )
@@ -35,19 +41,25 @@ class Recipe(models.Model):
         through='RecipeIngredients',
     )
     tags = models.ManyToManyField(to='Tag', related_name='recipe_tag')
-    image = models.BinaryField()
-    name = models.CharField()
-    text = models.TextField()
-    cooking_time = models.SmallIntegerField(
+    image = models.ImageField(upload_to='media/', verbose_name='Изображение')
+    text = models.TextField(verbose_name='Описание рецепта')
+    cooking_time = models.PositiveSmallIntegerField(
         verbose_name='Время приготовления (в минутах)',
-        default=1
+        default=1,
+        validators=[MinValueValidator(1)]
     )
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        verbose_name = 'Рецепт'
 
 
 class RecipeIngredients(models.Model):
     ingredient = models.ForeignKey(to='Ingredient', on_delete=models.CASCADE)
     recipe = models.ForeignKey(to='Recipe', on_delete=models.CASCADE)
     amount = models.FloatField(verbose_name='Количество')
+
+
+Ingredient._meta.get_field('name').verbose_name = 'Название ингридиента'
+Tag._meta.get_field('name').verbose_name = 'Название Тэга'
+Recipe._meta.get_field('name').verbose_name = 'Название рецепта'
+
