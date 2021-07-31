2ae2f568e7677cf976e7c6419de482bbbbee0ce5
Kurt Kostein <kurtkostein@yandex.ru>
2021-07-31 22:11:17 +0300

    fill recipe admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, Ingredient, Tag
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
