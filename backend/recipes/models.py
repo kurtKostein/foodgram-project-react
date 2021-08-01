be05ed0cdc6eae98d076afe728eac99cfedfe597
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-01 10:15:59 +0300

    add verbose_name_plural in Recipe models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -21,6 +21,10 @@ class Ingredient(BaseRecipeClass):
         max_length=200
     )
 
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
 
 class Tag(BaseRecipeClass):
     color = models.CharField(verbose_name='Цвет', max_length=200, null=True)
@@ -28,6 +32,7 @@ class Tag(BaseRecipeClass):
 
     class Meta:
         verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
 
 
 class Recipe(BaseRecipeClass):
@@ -51,15 +56,32 @@ class Recipe(BaseRecipeClass):
 
     class Meta:
         verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
 
 
 class RecipeIngredients(models.Model):
     ingredient = models.ForeignKey(to='Ingredient', on_delete=models.CASCADE)
     recipe = models.ForeignKey(to='Recipe', on_delete=models.CASCADE)
-    amount = models.FloatField(verbose_name='Количество')
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество',
+        default=1,
+        validators=[MinValueValidator(1)]
+    )
 
 
 Ingredient._meta.get_field('name').verbose_name = 'Название ингридиента'
 Tag._meta.get_field('name').verbose_name = 'Название Тэга'
 Recipe._meta.get_field('name').verbose_name = 'Название рецепта'
+
+
+class Favorite(models.Model):
+    ...
+
+
+class ShoppingCart(models.Model):
+    ...
+
+
+class Follow(models.Model):
+    ...
 
