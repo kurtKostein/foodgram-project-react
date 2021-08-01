97cc63a8a109729b845e8e8e13c01f01c58a101e
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-01 18:22:08 +0300

    add route to Ingredients

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from .models import *
+from .views import *
+
+router = DefaultRouter()
+
+router.register('ingredients', IngredientViewSet, 'ingredients')
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
