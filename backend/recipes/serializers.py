63c1cad42333f3a1e768c0584d9e1ae221ed4604
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-01 18:21:42 +0300

    fill serializers.py

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,9 +1,46 @@
-from .models import Ingredient
+from .models import (Ingredient, Tag, Recipe, ShoppingCart, RecipeIngredients,
+                     Favorite, Follow)
 
 from rest_framework import serializers
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
+        fields = '__all__'
+
+
+class RecipeIngredientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeIngredients
+        fields = '__all__'
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingCart
+        fields = '__all__'
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
         fields = '__all__'
