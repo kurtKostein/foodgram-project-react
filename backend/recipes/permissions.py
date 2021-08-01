92ef5bd60d8fd15a45cd26aee9fa182b79b57a13
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-01 22:14:58 +0300

    add IsAuthorOrAdminOrReadOnly permission in permissions.py

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -3,7 +3,17 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS or request.user.is_staff:
             return True
-        else:
-            return request.user.is_staff
+        return False
+
+
+class IsAuthorOrAdminOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if (
+                request.method in SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_staff
+        ):
+            return True
+        return False
