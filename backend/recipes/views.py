4a0c79f4d17b03a554ae1830232cf5d7645c5def
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-01 22:16:47 +0300

    start RecipeViewSet

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,14 +1,21 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, pagination
 
-from .models import Ingredient, Tag
-from .serializers import IngredientSerializer, TagSerializer
-from .permissions import IsAdminOrReadOnly
+from .models import Ingredient, Tag, Recipe
+from .serializers import IngredientSerializer, TagSerializer, RecipeSerializer
+from .permissions import IsAdminOrReadOnly, IsAuthorOrAdminOrReadOnly
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = (IsAuthorOrAdminOrReadOnly,)
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    pagination.PageNumberPagination.page_size = 20
     search_fields = ('name',)
 
 
