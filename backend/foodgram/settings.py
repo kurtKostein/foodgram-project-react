11eb3e6c49187af2f9696ff225fec4175daf88e6
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-04 20:37:32 +0300

    fix settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,8 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.authtoken'
-    'django-filter',
+    'rest_framework.authtoken',
+    'django_filters',
     'djoser',
 ]
 
