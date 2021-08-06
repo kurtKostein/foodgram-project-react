52c53ea6c7fda5be69c0f2e36cc9aa67f320a587
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-06 22:00:02 +0300

    create BaseRecipe abstract class, and Ingidient model in api/models.py and admin.site.register

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
+#  api/models.py
 from django.db import models
 
-# Create your models here.
+
+class BaseRecipeClass(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class Ingredient(BaseRecipeClass):
+    measurement_unit = models.CharField(
+        'Ед. измерения',
+        max_length=200
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
