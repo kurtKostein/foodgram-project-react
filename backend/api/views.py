ad217bb3ee012c2bb0c3a45e85938d8e4618bb5c
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-08 14:51:34 +0300

    write views for Ingredient and Tag

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,16 +2,13 @@ from rest_framework import viewsets
 
 from .models import Ingredient, Tag
 from .serializers import IngredientSerializer, TagSerializer
-from .permissions import IsAdminOrReadOnly
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = IsAdminOrReadOnly
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = IsAdminOrReadOnly
