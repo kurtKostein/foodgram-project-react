905ca04678ae860f935b30b4e41c5f3e2bda5f6f
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-08 14:51:15 +0300

    write views for Ingredient and Tag

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Ingredient, Tag
+from .serializers import IngredientSerializer, TagSerializer
+from .permissions import IsAdminOrReadOnly
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = IsAdminOrReadOnly
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = IsAdminOrReadOnly
