6aaee20902380d2b1e85a84eb4d73e9931875a5b
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-15 12:04:18 +0300

    rewrite favorite to @action

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,12 @@
 # from django.urls import include, path
 from rest_framework import routers
 
-from .views import IngredientViewSet, TagViewSet, RecipeViewSet, FavoriteRecipe
+from .views import IngredientViewSet, TagViewSet, RecipeViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'ingredients', IngredientViewSet)
 router.register(r'tags', TagViewSet)
 router.register(r'recipes', RecipeViewSet)
-router.register(r'recipes/(?<recip_id>\d+)/favorites', FavoriteRecipe)
 
 urlpatterns = router.urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,18 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 
 
 from .models import Ingredient, Tag, Recipe, FavoriteRecipe
+from users.models import CustomUser
 from .serializers import (
     IngredientSerializer,
     TagSerializer,
     RecipeSerializer,
     RecipeIngredientSerializer,
-    FavoriteRecipeSerializer
+    FavoriteRecipeSerializer,
+    RecipeForFavoritesSerializer,
 )
 from .permissions import IsAuthorOrAdminOrReadOnly
 
@@ -27,6 +31,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = (IsAuthorOrAdminOrReadOnly,)
+
+    @action(
+        detail=True,
+        methods=['get', 'delete'],
+        permission_classes=(IsAuthorOrAdminOrReadOnly,)  # TODO need IsOwner
+    )
+    def favorite(self, request, **kwargs):
+        recipe = self.get_object()
+        user = self.request.user
+        if request.method == 'GET':
+            favor = FavoriteRecipe.objects.create(recipe=recipe, user=user)
+            favor.save()
+            serializer = RecipeForFavoritesSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if request.method == 'DELETE':
+            favor = get_object_or_404(FavoriteRecipe, recipe=recipe, user=user)
+            favor.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class RecipeIngredientsViewSet(viewsets.ModelViewSet):
@@ -47,10 +69,9 @@ class RecipeIngredientsViewSet(viewsets.ModelViewSet):
         )
 
 
-class CreateDestroyViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin):
+class RetrieveDestroyViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     pass
-
-
-class FavoriteRecipeViewSet(CreateDestroyViewSet):
-    queryset = FavoriteRecipe.objects.all()
-    serializer_class = FavoriteRecipeSerializer
