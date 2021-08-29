f92d268bd7d35aecf8e3cbe35235ef4256e399f0
Kurt Kostein <kurtkostein@yandex.ru>
2021-08-29 12:53:13 +0300

    delete r from ingredient and tags router

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
-# from django.urls import include, path
+from django.urls import include, path
 from rest_framework import routers
 
-from .views import IngredientViewSet, TagViewSet, RecipeViewSet
-
+from .views import (IngredientViewSet, RecipeViewSet,
+                    TagViewSet)
 
 router = routers.DefaultRouter()
-router.register(r'ingredients', IngredientViewSet)
-router.register(r'tags', TagViewSet)
-router.register(r'recipes', RecipeViewSet)
+router.register('ingredients', IngredientViewSet)
+router.register('tags', TagViewSet)
+router.register(r'recipes', RecipeViewSet, basename='recipes')
 
-urlpatterns = router.urls
+urlpatterns = []
+
+urlpatterns += router.urls
