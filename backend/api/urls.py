ea2d270875a65ed67c27b6c17b869a32ca830fdb
Kurt Kostein <kurtkostein@yandex.ru>
2021-09-02 22:25:11 +0300

    add url path to favorite

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,13 +2,18 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import (IngredientViewSet, RecipeViewSet,
-                    TagViewSet)
+                    TagViewSet, FavoriteViewSet)
 
 router = routers.DefaultRouter()
 router.register('ingredients', IngredientViewSet)
 router.register('tags', TagViewSet)
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        'recipes/<int:recipe_id>/favorite/',
+        FavoriteViewSet.as_view({'get': 'create', 'delete': 'destroy'}),
+        name='favorite')
+]
 
 urlpatterns += router.urls
