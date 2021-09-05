c569e686ce2d188682ed154cc21ab180889d896f
Kurt Kostein <kurtkostein@yandex.ru>
2021-09-05 11:46:20 +0300

    rewrite favorite endpoint to APIview FavoriteVieSet left for make choose between them later

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import (IngredientViewSet, RecipeViewSet,
-                    TagViewSet, FavoriteViewSet)
+                    TagViewSet, FavoriteViewSet, FavoriteAPIView)
 
 router = routers.DefaultRouter()
 router.register('ingredients', IngredientViewSet)
@@ -10,10 +10,15 @@ router.register('tags', TagViewSet)
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
+    # path(
+    #     'recipes/<int:recipe_id>/favorite/',
+    #     FavoriteViewSet.as_view({'get': 'create', 'delete': 'destroy'}),
+    #     name='favorite')
     path(
         'recipes/<int:recipe_id>/favorite/',
-        FavoriteViewSet.as_view({'get': 'create', 'delete': 'destroy'}),
-        name='favorite')
+        FavoriteAPIView.as_view(),
+        name='favorite'
+    )
 ]
 
 urlpatterns += router.urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,13 @@
-from rest_framework import viewsets, mixins, permissions, status, response
+from rest_framework import mixins, permissions, response, status, viewsets
 from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
 
-
-from .models import Ingredient, Tag, Recipe
-from .serializers import (
-    IngredientSerializer,
-    TagSerializer,
-    RecipeSerializer,
-    RecipeIngredientSerializer,
-    CreateUpdateRecipeSerializer,
-    FavoriteRecipeSerializer,
-)
+from .models import Ingredient, Recipe, Tag
 from .permissions import IsAuthorOrAdminOrReadOnly
+from .serializers import (CreateUpdateRecipeSerializer,
+                          FavoriteRecipeSerializer, IngredientSerializer,
+                          RecipeIngredientSerializer, RecipeSerializer,
+                          TagSerializer)
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
@@ -45,7 +41,7 @@ class RecipeIngredientsViewSet(viewsets.ModelViewSet):
     search_fields = ('name',)
 
     def get_queryset(self):
-        pk = self.kwargs.get('review_id',)
+        pk = self.kwargs.get('review_id', )
         recipe = get_object_or_404(Recipe, pk=pk)
         return recipe.ingredients.all()
 
@@ -65,7 +61,7 @@ class CreateDestroyViewSet(
     pass
 
 
-class FavoriteViewSet(CreateDestroyViewSet):
+class FavoriteViewSet(CreateDestroyViewSet):  # Todo delete if apiview will be stayed
     pagination_class = None
     serializer_class = FavoriteRecipeSerializer
 
@@ -77,11 +73,41 @@ class FavoriteViewSet(CreateDestroyViewSet):
         recipe = self.kwargs.get('recipe_id')
         user = self.request.user.id
         data = {'user': user, 'recipe': recipe}
-        serializer = FavoriteRecipeSerializer(data=data, context={'request': request})
+        serializer = FavoriteRecipeSerializer(
+            data=data,
+            context={'request': request}
+        )
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return response.Response(serializer.data, status.HTTP_201_CREATED, headers=headers)
+        return response.Response(
+            serializer.data,
+            status.HTTP_201_CREATED,
+            headers=headers
+        )
 
     def destroy(self, request, *args, **kwargs):
         ...
+
+
+class FavoriteAPIView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, recipe_id):
+        data = {
+            'recipe': recipe_id,
+            'user': self.request.user.id
+        }
+        serializer = FavoriteRecipeSerializer(
+            data=data, context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status.HTTP_201_CREATED)
+
+    def delete(self, request, recipe_id):
+        user = self.request.user
+        favorite = get_object_or_404(user.favorites, recipe=recipe_id)
+        favorite.delete()
+        # recipe_id = self.kwargs.get('recipe_id')
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
