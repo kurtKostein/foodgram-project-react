a30d594317c23b4e770d35eae2d4b04f32ac8e0a
Kurt Kostein <kurtkostein@yandex.ru>
2021-10-10 22:37:17 +0300

    make pagination.py and change default pagination class in settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -98,8 +98,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6
+        'api.pagination.BasePagination',
 }
 
 DJOSER = {
@@ -130,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
