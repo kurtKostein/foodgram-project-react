5a98c428d0ce8194401aaa5f538accc83fde30ce
Kurt Kostein <kurtkostein@yandex.ru>
2021-10-12 20:15:58 +0300

    review refactoring api/admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,15 +4,29 @@ from .models import (FavoriteRecipe, Ingredient, Recipe, RecipeIngredients,
                      ShoppingCart, Subscription, Tag)
 
 
-@admin.register(ShoppingCart, Subscription)
-class UserToUserRelationsAdmin(admin.ModelAdmin):
-    pass
+class TagInlines(admin.TabularInline):
+    model = Recipe.tags.through
+    verbose_name = 'Тэг рецепта'
+    verbose_name_plural = 'Тэги рецепта'
+    extra = 1
+
+
+class RecipeIngredientsInline(admin.TabularInline):
+    model = Recipe.ingredients.through
+    fields = ('ingredient', 'amount')
+    extra = 1
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
+    fields = ('name', 'measurement_unit')
     list_filter = ('name',)
     search_fields = ('^name',)
+
+
+@admin.register(ShoppingCart, Subscription)
+class UserToUserRelationsAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Tag)
@@ -20,80 +34,16 @@ class TagAdmin(admin.ModelAdmin):
     pass
 
 
-class RecipeIngredientsAdmin(admin.TabularInline):
-    model = RecipeIngredients
-    fields = ('ingredient', 'amount')
-
-
-class TagInlines(admin.TabularInline):
-    verbose_name = Tag._meta.verbose_name
-    verbose_name_plural = Tag._meta.verbose_name_plural
-    model = Recipe.tags.through
-
-
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name',  'author', 'favorited')
+    list_display = ('name', 'author', 'cooking_time', 'favorited')
     list_filter = ('name', 'author')
+    exclude = ('tags',)
 
-    inlines = (RecipeIngredientsAdmin,)
+    inlines = (TagInlines, RecipeIngredientsInline)
 
     # noinspection PyMethodMayBeStatic
     def favorited(self, obj):
         return FavoriteRecipe.objects.filter(recipe=obj).count()
 
     favorited.short_description = 'В избранном'
-
-
-# class IngredientAdmin(admin.ModelAdmin):
-#     model = Ingredient
-#     list_display = ('name', 'measurement_unit')
-#     list_filter = ('name',)
-#     search_fields = ('^name',)
-
-#
-# class RecipeIngredientsInline(admin.TabularInline):
-#     model = RecipeIngredients
-
-#
-# class RecipeAdmin(admin.ModelAdmin):
-#     model = Recipe
-#     list_display = ('name', 'author', 'favorited')
-#     list_filter = ('name', 'author', 'tags')
-#     exclude = ('ingredients',)
-#
-#     inlines = (RecipeIngredientsInline,)
-#
-#     def favorited(self, obj):
-#         favorited_count = FavoriteRecipe.objects.filter(recipe=obj).count()
-#         return favorited_count
-#
-#     favorited.short_description = 'В избранном'
-#
-
-# class TagAdmin(admin.ModelAdmin):
-#     model = Tag
-#     list_display = ('name', 'color', 'slug')
-
-#
-# class ShoppingCartAdmin(admin.ModelAdmin):
-#     model = ShoppingCart
-#     list_display = ('user', 'recipe')
-#
-
-# class FavoriteRecipeAdmin(admin.ModelAdmin):
-#     model = FavoriteRecipe
-#     list_display = ('user', 'recipe')
-#
-
-# class SubscriptionAdmin(admin.ModelAdmin):
-#     model = Subscription
-#     list_display = ('author', 'subscriber')
-#
-
-# admin.site.register(Ingredient, IngredientAdmin)
-# admin.site.register(Recipe, RecipeAdmin)
-# admin.site.register(Tag, TagAdmin)
-# admin.site.register(ShoppingCart, ShoppingCartAdmin)
-# admin.site.register(FavoriteRecipe, FavoriteRecipeAdmin)
-# admin.site.register(Subscription, SubscriptionAdmin)
