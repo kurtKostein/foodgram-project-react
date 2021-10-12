1559ece966807f2145d4b7faa5377e6b753087f3
Kurt Kostein <kurtkostein@yandex.ru>
2021-10-12 20:17:07 +0300

    add forms.py and copy-paste CustomUserAdmin from article

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,43 @@
+#  users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import CustomUser
+from .forms import UserCreationForm, UserChangeForm
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = (
+        'email', 'username', 'first_name',
+        'last_name', 'is_staff', 'is_superuser'
+    )
+    list_filter = ('is_superuser',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'is_staff', 'is_superuser', 'password')}),
+        ('Personal info', {
+            'fields': ('username', 'first_name', 'last_name')
+        }),
+        ('Groups', {'fields': ('groups',)}),
+        ('Permissions', {'fields': ('user_permissions',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': (
+            'email', 'is_staff', 'is_superuser', 'password1', 'password2'
+        )}),
+        ('Personal info', {
+            'fields': ('username', 'first_name', 'last_name')
+        }),
+        ('Groups', {'fields': ('groups',)}),
+        ('Permissions', {'fields': ('user_permissions',)}),
+    )
+
+    search_fields = ('email', 'username', 'first_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
