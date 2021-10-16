c7f7e4b60598b4b46653129c3d8ff965beced177
Kurt Kostein <kurtkostein@yandex.ru>
2021-10-16 20:55:53 +0300

    return keyword for DATABASES

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -61,8 +61,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
